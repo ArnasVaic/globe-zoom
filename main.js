0d03096acbb1eb06af09edb209fc698523a4e534
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-09-30 18:23:40 +0300

    update script

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+const light = new THREE.PointLight(0xffffff, 1, 100)
+scene.add(light);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.SphereGeometry(1, 32, 16);
+const material = new THREE.MeshPhongMaterial({
+  color: 0x0000ff,
+  emissive: 0x000011
+});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 camera.position.z = 5;
 
+const clock = new THREE.Clock()
+
 function animate() {
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+  light.position.x = 2 * Math.sin(clock.getElapsedTime());
+  light.position.z = 2 * Math.cos(clock.getElapsedTime());
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 }
