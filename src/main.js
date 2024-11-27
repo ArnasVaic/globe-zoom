ca93b5e9c467afc0adea6519893198350e1c61eb
Domantas Keturakis <domantas.keturakis@vinted.com>
2024-11-27 17:23:13 +0200

    destytojo formule

--- src/main.js
+++ src/main.js
@@ -1,113 +1,120 @@
-import * as THREE from 'three';
-import vertexShader from "./assets/shaders/mobius-zoom.vert"
-import fragmentShader from "./assets/shaders/mobius-zoom.frag"
-import earth from "./assets/textures/earth.jpg"
+import * as THREE from "three";
+import vertexShader from "./assets/shaders/mobius-zoom.vert";
+import fragmentShader from "./assets/shaders/mobius-zoom.frag";
+import earth from "./assets/textures/earth.jpg";
 
 let scene, camera, renderer, sphere, clock;
 
-let zoom = 1
-const pole_0 = new THREE.Vector2(0.0, 0.0001) 
+let zoom = 1;
+const pole_0 = new THREE.Vector2(0.0, 0.0001);
 const pole = pole_0;
 
 // Initialize the scene
 function init() {
-    // Scene, Camera, Renderer
-    clock = new THREE.Clock();
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  // Scene, Camera, Renderer
+  clock = new THREE.Clock();
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+  );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    // Camera position
-    camera.position.z = 2;
+  // Camera position
+  camera.position.z = 2;
 
-    // Sphere with Shader Material
-    const geometry = new THREE.SphereGeometry(1, 512, 512);
-    const texture = new THREE.TextureLoader().load(earth);
-    const material = new THREE.ShaderMaterial({
-        uniforms: {
-            u_texture: { value: texture },
-            u_time: { value: 0 },
-            u_zoom: { value: zoom },
-            u_pole: { value: pole }
-        },
-        vertexShader: vertexShader,
-        fragmentShader: fragmentShader,
-        glslVersion: THREE.GLSL3,
-    });
+  // Sphere with Shader Material
+  const geometry = new THREE.SphereGeometry(1, 512, 512);
+  const texture = new THREE.TextureLoader().load(earth);
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      u_texture: { value: texture },
+      u_time: { value: 0 },
+      u_zoom: { value: zoom },
+      u_pole: { value: pole },
+      u_checked_pattern: { value: true },
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    glslVersion: THREE.GLSL3,
+  });
 
-    sphere = new THREE.Mesh(geometry, material);
-    sphere.rotateX(-Math.PI / 2)
-    scene.add(sphere);
+  sphere = new THREE.Mesh(geometry, material);
+  sphere.rotateX(-Math.PI / 2);
+  scene.add(sphere);
 
-    // Event Listeners
-    window.addEventListener('keydown', onKeyDown);
-    window.addEventListener('wheel', onScroll);
+  // Event Listeners
+  window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("wheel", onScroll);
 
-    clock.start()
-    animate();
+  clock.start();
+  animate();
 }
 
 // Key press event handler
 function onKeyDown(event) {
-    let speed = 0.01
+  let speed = 0.05;
 
-    switch (event.key) {
-        case 'ArrowRight':
-            sphere.rotation.z -= speed;
-            break;
-        case 'ArrowLeft':
-            sphere.rotation.z += speed;
-            break;
-        case 'ArrowUp':
-            sphere.rotation.x += speed;
-            break;
-        case 'ArrowDown':
-            sphere.rotation.x -= speed;
-            break;
-        case 'w':
-            pole.y -= speed;
-            break;
-        case 's':
-            pole.y += speed;
-            break;
-        case 'a':
-            pole.x -= speed;
-            break;
-        case 'd':
-            pole.x += speed;
-            break;
-        case 'Escape':
-            zoom = 1;
-            pole.x = pole_0.x;
-            pole.y = pole_0.y;
-            sphere.rotation.x = -Math.PI / 2;
-            sphere.rotation.z = 0;
-            break;
-    }
+  switch (event.key) {
+    case "ArrowRight":
+      sphere.rotation.z -= speed;
+      break;
+    case "ArrowLeft":
+      sphere.rotation.z += speed;
+      break;
+    case "ArrowUp":
+      sphere.rotation.x += speed;
+      break;
+    case "ArrowDown":
+      sphere.rotation.x -= speed;
+      break;
+    case "w":
+      pole.y -= speed;
+      break;
+    case "s":
+      pole.y += speed;
+      break;
+    case "a":
+      pole.x -= speed;
+      break;
+    case "d":
+      pole.x += speed;
+      break;
+    case "Escape":
+      zoom = 1;
+      pole.x = pole_0.x;
+      pole.y = pole_0.y;
+      sphere.rotation.x = -Math.PI / 2;
+      sphere.rotation.z = 0;
+      break;
+  }
 
-    //pole.y = clamp(pole.y, 0 + 0.0001, Math.PI - 0.0001);
-    sphere.material.uniforms.u_pole.value = pole
+  //pole.y = clamp(pole.y, 0 + 0.0001, Math.PI - 0.0001);
+  sphere.material.uniforms.u_pole.value = pole;
 }
 
 function clamp(value, min, max) {
-    return Math.min(Math.max(value, min), max);
+  return Math.min(Math.max(value, min), max);
 }
 
 function onScroll(event) {
-    let speed = 0.001
-    zoom -= speed * event.deltaY;
-    zoom = clamp(zoom, 1, 3)
-    sphere.material.uniforms.u_zoom.value = zoom
+  let speed = 0.001;
+  zoom -= speed * event.deltaY;
+  // zoom = clamp(zoom, 1, 3)
+  zoom = clamp(zoom, -4, 4);
+  sphere.material.uniforms.u_zoom.value = zoom;
 }
 
 // Animation loop
 function animate() {
-    sphere.material.uniforms.u_time.value = clock.getElapsedTime();
+  sphere.material.uniforms.u_time.value = clock.getElapsedTime();
 
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
 init();
